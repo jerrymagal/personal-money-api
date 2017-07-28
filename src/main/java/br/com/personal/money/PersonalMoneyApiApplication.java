12be4476f55b174128e4715df0681f163d1037d2
Alexandre Guimarães Rolim <alexandreg.rolim@saude.gov.br>
2017-07-28 14:44:56 -0300

    Comitando o projeto

---
+++ src/main/java/br/com/personal/money/PersonalMoneyApiApplication.java
@@ -0,0 +1,12 @@
+package br.com.personal.money;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PersonalMoneyApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PersonalMoneyApiApplication.class, args);
+	}
+}
