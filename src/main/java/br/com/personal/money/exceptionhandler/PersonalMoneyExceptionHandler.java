44455d9c02da44b9f85b85c6f37b356b25f4ca82
Alexandre Guimarães Rolim <alexandreg.rolim@saude.gov.br>
2017-07-31 16:57:01 -0300

    Implementando delete

--- src/main/java/br/com/personal/money/exceptionhandler/PersonalMoneyExceptionHandler.java
+++ src/main/java/br/com/personal/money/exceptionhandler/PersonalMoneyExceptionHandler.java
@@ -3,6 +3,7 @@ package br.com.personal.money.exceptionhandler;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -11,6 +12,7 @@ import org.springframework.validation.BindingResult;
 import org.springframework.validation.FieldError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
@@ -40,7 +42,19 @@ public class PersonalMoneyExceptionHandler extends ResponseEntityExceptionHandle
 		
 		return handleExceptionInternal(ex, listaErros, headers, HttpStatus.BAD_REQUEST, request);
 	}
-
+	
+	@ExceptionHandler({EmptyResultDataAccessException.class})
+	public ResponseEntity<?> handleEmptyResultDataAccessException(EmptyResultDataAccessException ex, WebRequest request) {
+		
+		String mensagemUsuario = MensagensUtil.getMessage("recurso.nao-encontrado");
+		String mensagemDesenvolvedor = ex.toString();
+		
+		List<Erro> listaErros = criarListaErros(null);
+		listaErros.add(new Erro(mensagemUsuario, mensagemDesenvolvedor));
+		return handleExceptionInternal(ex, listaErros, new HttpHeaders(), HttpStatus.NOT_FOUND, request);
+		
+	}
+	
 	private List<Erro> criarListaErros(BindingResult result) {
 		
 		List<Erro> listaErros = new ArrayList<>();
